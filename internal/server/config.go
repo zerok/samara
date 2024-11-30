f134a08aee4607b635d38b6da625456f342d2cbc
Horst Gutmann <zerok@zerokspot.com>
2024-11-30 16:15:22 +0100

    fix: allow raw handles for incoming URIs (#7)

--- internal/server/config.go
+++ internal/server/config.go
@@ -4,5 +4,6 @@ import "github.com/bluesky-social/indigo/xrpc"
 
 type Configuration struct {
 	AllowedRootAccountDIDs []string
+	AllowedRootAccounts    []string
 	Client                 *xrpc.Client
 }
